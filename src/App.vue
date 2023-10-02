0d004e3452b876c9978a25dd8b1cf72c47a3a920
Aleksandra Tremba <aleksandra.tremba@gmail.com>
2023-10-02 13:16:13 +0300

    Copied code to Timer.vue and connected it with App.vue for testing purposes

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import TimerComponent from './components/Timer.vue';
+
 </script>
 
 <template>
@@ -6,34 +8,13 @@
   </header>
 
   <main>
+    <main>
+      <div class="mx-auto p-2" style="width: 280px;">
+        <TimerComponent />
+      </div>
+    </main>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
